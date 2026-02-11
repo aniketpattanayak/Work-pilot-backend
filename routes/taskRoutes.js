c9f2c3d92fb6aec84ec3ec5c0b04ae0cf09aa3f8
Aniket Pattanayak <82846434+aniketpattanayak@users.noreply.github.com>
2026-02-11 12:14:50 +0530

    Update taskRoutes.js

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -57,7 +57,7 @@ router.put('/assign-coordinator', assignToCoordinator);
 router.get('/company-overview/:tenantId', getCompanyOverview);
 // --- DELEGATION PROTOCOLS ---
 router.post('/create-task', upload.array('files'), taskController.createTask);
-router.delete('/task/:taskId', taskController.deleteTask);
+router.delete('/:taskId', taskController.deleteTask);
 router.post('/handle-revision', taskController.handleRevision);
 router.post('/coordinator-force-done', taskController.coordinatorForceDone);
 // --- ANALYTICS & SCOREBOARDS ---
